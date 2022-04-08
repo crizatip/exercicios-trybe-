2ecb493c09970ae87cbe3f4d0ff9b5cc49023d87
cristiano <crizatip@gmail.com>
2022-04-07 21:32:21 -0300

    Primeiro programa finalizado

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa01.js
@@ -0,0 +1,14 @@
+// Programa de operações matematicas.
+
+const a = 2;
+const b = 2;
+
+console.log(a + b);
+
+console.log(a - b);
+
+console.log(a * b);
+
+console.log(a / b);
+
+console.log(a % b);
