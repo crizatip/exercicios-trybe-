46400cca2cbb9be0497c2f6508d1400a9cbe9b74
cristiano <crizatip@gmail.com>
2022-04-07 22:32:37 -0300

    Quinto  programa finalizado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa05.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa05.js
@@ -1,3 +1,13 @@
-const a =50;
-const b =40;
-const c =90;
+const anguloA =50;
+const anguloB =50;
+const anguloC =90;
+
+if (anguloA + anguloB + anguloC == 180){
+    console.log(true)
+} else if(anguloA + anguloB + anguloC > 180){
+    console.log(false)
+} else if(anguloA + anguloB + anguloC < 180){
+    console.log(false)
+} else{
+    console.log("ERRO")
+};
