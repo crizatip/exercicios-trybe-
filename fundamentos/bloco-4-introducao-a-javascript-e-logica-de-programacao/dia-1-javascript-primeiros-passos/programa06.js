1cebcc1386dfbb95be29f4fe23237cc328f4b9ee
cristiano <crizatip@gmail.com>
2022-04-08 13:12:06 -0300

    programa 6 finalizado

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa06.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/programa06.js
@@ -1,5 +1,35 @@
-let peca = "BIsPo";
+let piece = "Rei";
 
-let pecalowercase = peca.toLowerCase()
+let piecelowercase = piece.toLowerCase(); //transforma texto em minusculo
 
-console.log(pecalowercase)
+switch(piecelowercase){
+    case "bispo":
+        console.log("Diagonal");
+        break;
+    
+    case "peão":
+        console.log("De 1 a 2 casas para frente");
+        break;
+
+    case "cavalo":
+        console.log("movimentos em L pode pular peças");
+        break;
+
+    
+    case "rainha":
+        console.log("todas as direções quantas casas quiser");
+        break;
+
+    case "torre":
+        console.log("Anda em linha reta quantas casas quiser");
+        break;
+    
+
+    case "rei":
+        console.log("uma casa em todas as direções");
+        break;
+        
+    default: 
+    console.log("ERRO") 
+    break;  
+}
