bf20e757ad89d880a8d5b65b670b39ab1954ae6f
cristiano <crizatip@gmail.com>
2022-04-16 18:27:11 -0300

    acessado elemento filho apartir do pai

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/fixacao/script.js
@@ -1,3 +1,5 @@
 document.getElementById("elementoOndeVoceEsta").parentNode.style.color = "red"
 
 document.getElementById("elementoOndeVoceEsta").firstElementChild.innerHTML = "Filho do Filho"
+
+document.getElementById("pai").firstElementChild
