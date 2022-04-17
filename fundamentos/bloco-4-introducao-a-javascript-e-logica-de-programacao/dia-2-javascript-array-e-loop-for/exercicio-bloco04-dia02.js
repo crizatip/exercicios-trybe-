61a031808f22786f047701abf85071c0b2b982e0
cristiano <crizatip@gmail.com>
2022-04-17 18:36:49 -0300

    exercicio bloco 4 dia 2 terceiro  exercicio

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-bloco04-dia02.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicio-bloco04-dia02.js
@@ -21,10 +21,21 @@ resultado = numbers[i] + resultado
 console.log(resultado)
 
 
-
 // Para o terceiro exercício, calcule e imprima a média aritmética dos valores contidos no array;
 // A média aritmética é o resultado da soma de todos os elementos divido pelo número total de elementos.
 
+
+let resultado2 = 0
+
+for(let i = 0; i < numbers.length; i+=1){
+
+resultado2 = numbers[i] + resultado2
+
+};
+
+let media = resultado2 / numbers.length
+
+console.log(media)
 
 // Com o mesmo código do exercício anterior, caso o valor final seja maior que 20, imprima a mensagem: "valor maior que 20". Caso não seja, imprima a mensagem: "valor menor ou igual a 20";
 
