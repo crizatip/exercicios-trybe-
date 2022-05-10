ebe95445aed0f5848d07aa29b79e87da403a0615
cristiano <crizatip@gmail.com>
2022-05-10 11:59:48 -0300

    exercicio bloco 7.1.2 completo

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,17 +1,4 @@
-// function testingScope(escopo) {
-//     if (escopo === true) {
-//       var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-//       ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-//       console.log(ifScope);
-//     } else {
-//       var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-//       console.log(elseScope);
-//     }
-//     console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-//   }
-
-//   testingScope(true);
-
+///////////////////////// 1.exercicio /////////////////////////////////////////////
 
 const testingScope = escopo => {
     if (escopo === true) {
@@ -25,3 +12,13 @@ const testingScope = escopo => {
   };
 
   testingScope(true);
+
+  ///////////////////////// 2.exercicio /////////////////////////////////////////////
+
+  const comparacao = (a, b) => {return (a - b)}
+
+
+  const oddsAndEvens = Array => Array.sort(comparacao);
+
+  
+console.log(oddsAndEvens([13, 3, 4, 10, 7, 2]));
